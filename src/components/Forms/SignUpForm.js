f26ba55978c5045b77674a40d07e2d62b33a797d
Haingo <haingo.razafindranto@gmail.com>
2024-08-05 18:26:28 -0400

    Sign up // implement account creation feature

--- src/components/Forms/SignUpForm.js
+++ src/components/Forms/SignUpForm.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Form.scss';
+import axios from 'axios';
 
 function SignUpForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [feedback, setFeedback] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        axios.post('http://localhost:8080/signup', {
+            email: email,
+            password: password
+        })
+        .then(res => {
+            if (res.data === "Success") {
+                setFeedback("Account created successfully. You can now log in")
+                navigate('/')
+            } else {
+                setFeedback("An error occurred while creating your account. Please try again.") 
+            }
+        })
+        .catch(err => console.log(err))
+    }
 
     return (
-        <form className="accountForm">
+        <form className="accountForm" onSubmit={handleSubmit}>
             <h1 className="formTitle">CREATE A NEW ACCOUNT</h1>
             <p>Already have one? <Link to="/">Log in here</Link></p>
             <div className="formSection">
-                <p className="formLabel">Name<span>*</span></p>
-                <input type="text" name="loginName"/>
-            </div>
-            <div className="formSection">
                 <p className="formLabel">Email<span>*</span></p>
-                <input type="email" name="loginEmail"/>
+                <input type="email" name="loginEmail" onChange={e => setEmail(e.target.value)} required/>
             </div>
             <div className="formSection">
                 <p className="formLabel">Password<span>*</span></p>
-                <input type="password" name="loginPass"/>
+                <input type="password" name="loginPass" onChange={e => setPassword(e.target.value)} required/>
             </div>
-            <button className="formButton"><Link to="/home"><b>Join today</b></Link></button>
+            <button className="formButton" type="submit">Join today</button>
+            {feedback && <p>{feedback}</p>}
         </form>
     );
 }
