9d1020e6dd2b4736504f7b8a507314114d0d17d9
Haingo <haingo.razafindranto@gmail.com>
2024-09-02 12:32:24 -0400

    Profile // Allow users to update their email

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,83 +1,144 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./Profile.scss";
 import Navbar from '../../components/Navbar/Navbar';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../redux/reducer/userSlice';
+import axios from 'axios';
 
 function Profile(props) {
 
-    const [formInputData, setFormInputData] = useState([])
-    const [feedback, setFeedback] = useState("")
-    const [formData, setFormData] = useState("password")
+    const [emailChangeData, setEmailChangeData] = useState([]);
+    const [passwordChangeData, setPasswordChangeData] = useState([]);
+    const [feedback, setFeedback] = useState("");
+    const [formData, setFormData] = useState("password");
 
-    const loggedInUser = useSelector((state) => state.user.email);
-    const navigate = useNavigate(); 
+    // const loggedInUser = useSelector((state) => state.user.email);
+    const loggedInUser = localStorage.getItem("user_email");
+    const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const handleSubmit  = (e) => {  
+    const onEmailChange = (e) => {
+        const newInput = (data) => ({...data, [e.target.name]: e.target.value});
+        setEmailChangeData(newInput);
+    }
+
+    const onEditEmailSubmit = (e) => {
+        e.preventDefault();
+        if (window.confirm("Are you sure you want to change your email?")) {
+            axios.put(process.env.REACT_APP_API_URL + '/update-email', {
+                email: loggedInUser,
+                newEmail: emailChangeData.newEmail,
+                password: emailChangeData.password
+            })
+            .then (res => {
+                console.log(res.data);
+                if (res.data === "Success") {
+                    setFeedback("Email updated successfully")
+                    localStorage.setItem("user_email", emailChangeData.newEmail);
+                } else {
+                    setFeedback("Invalid password. Please try again.")
+                }
+            })
+            .catch(err => setFeedback(err))
+        } else {
+            setFeedback("Email update cancelled")
+            return;
+        }
+    }
+
+    const onEditPasswordSubmit = (e) => {
+        e.preventDefault();
     }
 
     const onLogout = () => {
-        if(window.confirm("Are you sure you want to log out?")){
+        if (window.confirm("Are you sure you want to log out?")) {
             localStorage.removeItem("user_email");
             dispatch(logoutUser());
-            navigate("/")
+            navigate("/");
         }
     }
 
     const onDeleteAccount = (e) => {
-    }   
+        // Handle account deletion
+    }
 
     return (
         <>
-            <Navbar/>
-            <div className="profileContainer">        
+            <Navbar />
+            <div className="profileContainer">
                 <section className="profileSection">
 
-                    <form action="POST" className="accountForm" onSubmit={handleSubmit}>
+                    <div className="accountForm">
                         <h1 className="formTitle">My profile</h1>
-                        <p>Update your email or password</p>
-                        {/* Show logged in user's email here */}
                         <div className="formSection">
                             <p className="formLabel">Email</p>
                             <input type="email" id="email" name="email" required value={loggedInUser} readOnly></input>
                         </div>
 
                         <div>
-                            {formData === "email" ? 
-                            <button className="formButton" onClick={() => setFormData("password")}>Update password instead</button>:
-                            <button className="formButton" onClick={() => setFormData("email")}>Update email instead</button>
+                            {formData === "email" ?
+                                <button className="formButton" onClick={() => setFormData("password")}>Update password instead</button> :
+                                <button className="formButton" onClick={() => setFormData("email")}>Update email instead</button>
                             }
                         </div>
 
-                        { formData === "email" ? 
-                            <div className="formSection">
-                                <p className="formLabel">New Email</p>
-                                <input type="newEmail" id="newEmail" name="newEmail" required></input>
-                            </div> :
-                            <>
+                        {formData === "email" ?
+                            <form action="POST" className="accountForm" onSubmit={onEditEmailSubmit}>
+                                <p>Update your email</p>
+
+                                <div className="formSection">
+                                    <p className="formLabel">New Email*</p>
+                                    <input 
+                                        type="email" 
+                                        id="newEmail" 
+                                        name="newEmail" 
+                                        value={emailChangeData.newEmail}
+                                        onChange = {onEmailChange}
+                                        required
+                                    />
+                                </div>
+
+                                <div className="formSection">
+                                    <p className="formLabel">Password<span>*</span></p>
+                                    <input 
+                                        type="password" 
+                                        id="password" 
+                                        name="password" 
+                                        value={emailChangeData.password}
+                                        onChange = {onEmailChange}
+                                        required
+                                    />
+                                </div>
+
+                                <div id="workout-form-feedback">{feedback}</div>
+                                <button type="submit" className="formButton">Update</button>
+                            </form>
+                            :
+                            <form action="POST" className="accountForm" onSubmit={onEditPasswordSubmit}>
+                                <p>Update your password</p>
+
                                 <div className="formSection">
                                     <p className="formLabel">New Password<span>*</span></p>
                                     <input type="newPassword" id="newPassword" name="newPassword" required></input>
                                 </div>
-            
+
                                 <div className="formSection">
                                     <p className="formLabel">Confirm password<span>*</span></p>
                                     <input type="confirmPassword" id="confirmPassword" name="confirmPassword" required></input>
                                 </div>
-                            </>
+
+                                <div className="formSection">
+                                    <p className="formLabel">Password<span>*</span></p>
+                                    <input type="password" id="password" name="password" required></input>
+                                </div>
+
+                                <div id="workout-form-feedback">{feedback}</div>
+
+                                <button type="submit" className="formButton">Update</button>
+                            </form>
                         }
-                        <div className="formSection">
-                            <p className="formLabel">Password<span>*</span></p>
-                            <input type="password" id="password" name="password" required></input>
-                        </div>
-                    
-                        <div id="workout-form-feedback">{feedback}</div>
-
-                        <button type="submit" className="formButton">Update</button>
-                    </form>
-
+                    </div>
                     <form action="" className="accountForm" onSubmit={onLogout}>
                         <h1 className="formTitle">Log out</h1>
                         <button className="formButton" type="submit"><b>Logout</b></button>
@@ -90,7 +151,7 @@ function Profile(props) {
                         {feedback && <p>{feedback}</p>}
                     </form>
 
-                </section>  
+                </section>
             </div>
         </>
     );
