efcdc682b9005795fff44135849215b66591eff3
Haingo <haingo.razafindranto@gmail.com>
2024-12-18 18:31:41 -0500

    Add one chatbot question

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,5 @@
 export const chatbotQuestions = [
   'How many minutes have I spent working out since I started my journey?',
   'How many workout programs have I completed so far?',
+  "How many days have I worked out since I started my journey?"
 ];
